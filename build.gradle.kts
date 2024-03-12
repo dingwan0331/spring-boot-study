ced05ebbe83331626c669baa3ea0181b0feaf4cb
joey <ding@joeys-MacBook-Air.local>
2024-03-12 23:15:45 +0900

    chore: 스피링 기초 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version "3.0.4"
 }
 
 group = "org.example"
@@ -10,7 +12,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test"){
+        exclude(group= "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
 tasks.test {
@@ -19,3 +25,5 @@ tasks.test {
 kotlin {
     jvmToolchain(17)
 }
+
+java.sourceCompatibility = JavaVersion.toVersion("17")
